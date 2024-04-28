fd3685b196b5cd60ffbaeebf7a742af2c98797c8
Natalia <icxodnik@gmail.com>
2024-04-28 03:23:02 -0400

    add voteName

--- src/components/FeedbackOptions/index.jsx
+++ src/components/FeedbackOptions/index.jsx
@@ -1,14 +1,14 @@
 import css from './index.module.css';
 import React, { Component } from 'react';
-import { Votes } from 'shared/enum';
+import { Votes, voteName } from 'shared/enum';
 
 export default class FeedbackOptions extends Component {
   render() {
     return (
       <div className="feedbackOptions">
-        <button onClick={() => this.props.addVote(Votes.good)}>Good</button>
-        <button onClick={() => this.props.addVote(Votes.neutral)}>Neutral</button>
-        <button onClick={() => this.props.addVote(Votes.bad)}>Bad</button>
+        <button onClick={() => this.props.addVote(Votes.good)}>{voteName(Votes.good)}</button>
+        <button onClick={() => this.props.addVote(Votes.neutral)}>{voteName(Votes.neutral)}</button>
+        <button onClick={() => this.props.addVote(Votes.bad)}>{voteName(Votes.bad)}</button>
       </div>
     );
   }
--- src/shared/enum.js
+++ src/shared/enum.js
@@ -3,3 +3,13 @@ export const Votes = {
     neutral: 'neutral',
     bad: 'bad',
 };
+
+const VoteName = {
+    good: 'Good',
+    neutral: 'Neutral',
+    bad: 'Bad',
+}
+
+export function voteName(vote) {
+    return VoteName[vote];
+}
